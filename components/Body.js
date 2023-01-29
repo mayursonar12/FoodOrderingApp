7307580382bd17df330569cf82f383b8c660e4e6
mayursonar07 <mayursonar07@gmail.com>
2023-01-30 00:47:12 +0530

    Contexts

--- components/Body.js
+++ components/Body.js
@@ -1,12 +1,54 @@
 import RestaurantsList from "./RestaurantsList"
+import {Context } from "./Context.js"
+import { useState } from "react";
 
 
 const Body = () => {
+    const[searchText, setSearchText] = useState("");
+    const[restaurantList, setRestaurantList] = useState([]);
+    const[filteredRestoList, setFilteredRestoList] = useState([]);
+
+    onSearchTextChange = (e) => {
+        setSearchText(e.target.value);
+
+        if (e.target.value.length > 2){
+            let filteredList = restaurantList.filter((restaurantInfo)=>{
+                // Extract name of each restaurant.
+                // Match that name against the search string
+                let match = restaurantInfo.name.match(new RegExp(e.target.value,"i"));
+                if (match) return restaurantInfo;
+            });
+            console.log(filteredList);
+            setFilteredRestoList(filteredList);
+        } else {
+            setFilteredRestoList(restaurantList);
+        }
+    }
+
+    onSearchBtnClick = () => {
+        // console.log(searchText);
+        // console.log(restaurantList);
+
+        let filteredList = restaurantList.filter((restaurantInfo)=>{
+            // Extract name of each restaurant.
+            // Match that name against the search string
+            let match = restaurantInfo.name.match(new RegExp(searchText,"i"));
+            if (match) return restaurantInfo;
+        });
+        console.log(filteredList);
+        setFilteredRestoList(filteredList);
+    }
+
     return (
         <>
             {/* This body component should display all restaaurnts near me */}
-
-            <RestaurantsList/>
+            <div className="search">
+                <input value={searchText} onChange={onSearchTextChange} type="text" placeholder="Search"></input>
+                <button onClick={onSearchBtnClick}>Search</button> 
+            </div>
+            <Context.Provider value={{restaurantList, filteredRestoList, setRestaurantList}}>
+                <RestaurantsList/>
+            </Context.Provider>
         </>
     )
 }
--- components/RestaurantsList.js
+++ components/RestaurantsList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Shimmer from "./Shimmer";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { Context } from "./Context";
 import {ALL_RESTAURANTS_LIST} from "../Config.js"
 import RestaurantCards from "./RestaurantCards";
 
 const RestaurantsList = () => {
 
 //    const restaurantlist = ALL_RESTAURANTS_LIST.cards;
+    const Contexts = useContext(Context);
+    console.log(Contexts);
 
-    const [restaurantlist, setRestList] = useState([]);
-
-    ALL_RESTAURANTS_LIST.cards.forEach(restaurant => {
-        console.log(restaurant.data);
-    });
+    // // Since we are now using Context to get the parent state variables.
+    // // We will discontinue to use local state variables
+    // const [restaurantlist, setRestList] = useState([]);
 
     // Use effect hook takes a function argument
     // it will call this function depending on the second argument of useeffect hook
@@ -21,41 +22,24 @@ const RestaurantsList = () => {
 
         let rlist = [];
         ALL_RESTAURANTS_LIST.cards.forEach(restaurant => {
-            console.log(restaurant.data);
+            //console.log(restaurant.data);
             rlist.push(restaurant.data);
         });
         if (rlist.length)
-            setRestList(rlist);
+            Contexts.setRestaurantList(rlist);
 
         // useeffect is called later than the other code.
         // useeffect will update the latest restaurant list
         // SO its better to update a state from useeffect so that the re-render is called 
-        // 
-
-        // Call API to get list of all restauants
-        // const url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.59937&lng=73.900618&page_type=DESKTOP_WEB_LISTING";
-        // const url1 = "https://www.swiggy.com/dapi/menu/v4/full?lat=18.59937&lng=73.900618&menuId=22472";
-        // const fetchData = async () => {
-        //     try {
-        //         const response = await fetch(url1,{
-        //             mode:'no-cors'
-        //         });
-        //         const json = await response.json();
-        //         console.log(json);
-        //     } catch (error) {
-        //         console.log("error", error);
-        //     }
-        // };
-        // fetchData();
 
     },[]) 
-
-    if (restaurantlist.length) {
+    let restaurantList = Contexts.filteredRestoList.length ? Contexts.filteredRestoList : Contexts.restaurantList;
+    if (restaurantList.length) {
         let names = [];
-        restaurantlist.forEach((restaurant)=>{
+        restaurantList.forEach((restaurant)=>{
             names.push(restaurant.name);
         })
-        var cards = restaurantlist.map((r)=>{
+        var cards = restaurantList.map((r)=>{
             return (<RestaurantCards restaurant={r}/>)
         })
         
@@ -72,16 +56,5 @@ const RestaurantsList = () => {
             </>
         )
     }
-
-    // return (
-    //     <>
-    //     {/* List of all restaurants near me */}
-    //     <Shimmer/>
-
-    //     </>
-    // )
-
 }
-
 export default RestaurantsList;
-
